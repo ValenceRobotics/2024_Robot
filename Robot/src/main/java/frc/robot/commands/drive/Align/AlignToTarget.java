4b5551ac65320ed67a508202cffc9884f3c914b3
Valence <valencerobotics@gmail.com>
2024-02-27 21:21:25 -0500

    Add target align skeleton code

--- Robot/src/main/java/frc/robot/commands/drive/Align/AlignToTarget.java
+++ Robot/src/main/java/frc/robot/commands/drive/Align/AlignToTarget.java
@@ -4,21 +4,45 @@
 
 package frc.robot.commands.drive.Align;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.Command;
 
 public class AlignToTarget extends Command {
   /** Creates a new AlignToTarget. */
+
+  Pose2d target = null; 
   public AlignToTarget() {
     // Use addRequirements() here to declare subsystem dependencies.
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    var alliance = DriverStation.getAlliance();
+    boolean isRed = false; 
+    if (alliance.isPresent()) {
+      isRed = alliance.get() == DriverStation.Alliance.Red;
+    }
+
+    if(isRed){
+      target = new Pose2d(0.25, 5.53, new Rotation2d(0));
+    } else{
+      target = new Pose2d(16.29, 5.53, new Rotation2d(0));
+    }
+
+
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+      // calculate desired heading from current pose
+      // reach it
+
+
+  }
 
   // Called once the command ends or is interrupted.
   @Override
